004aaa87fa11c5a72ad7122f6e50d4b81e3a8eb1
7Tech <119029097+Se1TECH@users.noreply.github.com>
2022-11-27 14:40:40 +0530

    Create convert decimal value to Paisa.c

---
+++ convert decimal value to Paisa.c
@@ -0,0 +1,14 @@
+//Que : Write program to convert decimal value to Paisa
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float dec;
+	int val;
+	clrscr();
+	printf("Enter the value of decimal number : ");
+	scanf("%f",&dec);
+	val = 100*dec;
+	printf("\Return value of decimal to paisa is  :  %d",val);
+	getch();
+}
