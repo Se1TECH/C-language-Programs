5545589e69b4f6c25605f5937fd25871709fcb9f
7Tech <119029097+Se1TECH@users.noreply.github.com>
2022-12-02 18:09:13 +0530

    Create Number is even or odd.c

---
+++ Number is even or odd.c
@@ -0,0 +1,26 @@
+//Que: Find entered number is even or odd
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n1;
+	clrscr();
+	printf("Enter value of n1: ");
+	scanf("%d",&n1);
+	if(n1==0)
+	{
+		printf("%d is ZERO",n1);
+	}
+	else
+	{
+		if(n1%2==0)
+		{
+			printf("%d is EVEN",n1);
+		}
+		else
+		{
+			printf("%d id ODD",n1);
+		}
+	}
+	getch();
+}
