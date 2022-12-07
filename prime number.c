cc03486262c24db5186e8a1a5bd58a8a112267e9
7Tech <119029097+Se1TECH@users.noreply.github.com>
2022-12-07 21:56:47 +0530

    Create prime number.c

---
+++ prime number.c
@@ -0,0 +1,26 @@
+//Question: Write Program in c to check entered number is prime or not.
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int no,count=0,i;
+	clrscr();
+	printf("Enter Value : ");
+	scanf("%d",&no);
+	for(i=1;i<=no;i++)
+	{
+		if(no%i==0)
+		{
+			count++;
+		}
+	}
+	if(count==2)
+	{
+		printf("%d is Prime Number",no);
+	}
+	else
+	{
+		printf("%d is not Prime Number",no);
+	}
+	getch();
+}
