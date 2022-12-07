7ac8505aac771e5c189698741aefabd2b7d6b937
7Tech <119029097+Se1TECH@users.noreply.github.com>
2022-12-07 21:55:44 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,17 @@
+//Question: Print factorial of number
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int fact=1,i,no;
+	clrscr();
+	printf("Enter number for factorial: ");
+	scanf("%d",&no);
+	for(i=1;i<=no;i++)
+	{
+		fact=fact*i;
+	}
+	printf("Facctorial of %d is : %d",no,fact);
+	getch();
+
+}
