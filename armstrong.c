e67eb4c9ef2b30c2a0f94a6aef257e6e73c0e87c
7Tech <119029097+Se1TECH@users.noreply.github.com>
2022-12-08 15:00:23 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,26 @@
+//Quetion: Write Program in C to Check Entered number is armstrong or not
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,temp,rem,sum=0;
+	clrscr();
+	printf("Enter value : ");
+	scanf("%d",&n);
+	temp=n;
+	while(temp!=0)
+	{
+		rem=temp%10;
+		sum=sum+rem*rem*rem;
+		temp=temp/10;
+	}
+	if(sum==n)
+	{
+		printf("\n%d is armstrong number",n);
+	}
+	else
+	{
+		printf("\n%d is not armstrong number",n);
+	}
+	getch();
+}
