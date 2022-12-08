28ccee73f2505895799dde35cb3375717036651e
7Tech <119029097+Se1TECH@users.noreply.github.com>
2022-12-08 15:01:55 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,26 @@
+//Question: Write Program in C to Check Entered Number is Palindrome or Not
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,sum=0,rem,temp;
+	clrscr();
+	printf("Enter your value: ");
+	scanf("%d",&n);
+	temp=n;
+	while(n!=0)
+	{
+		rem=n%10;
+		sum=sum*10+rem;
+		n=n/10;
+	}
+	if(sum==temp)
+	{
+		printf("%d is palidrome",temp);
+	}
+	else
+	{
+		printf("%d is not palidrome",temp);
+	}
+	getch();
+}
