1476ef8871b50d05328cace50719195d902ee335
7Tech <119029097+Se1TECH@users.noreply.github.com>
2022-12-09 15:44:57 +0530

    Create maximum number from array.c

---
+++ maximum number from array.c
@@ -0,0 +1,22 @@
+//Question: Write Program in C to find Maximum from array.
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int arr[10],i;
+	clrscr();
+	printf("Enter 10 Numbers: ");
+	for(i=0;i<10;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<10;i++)
+	{
+		if(arr[0]<arr[i])
+		{
+			arr[0]=arr[i];
+		}
+	}
+	printf("\nMaximum value of array: %d",arr[0]);
+	getch();
+}
