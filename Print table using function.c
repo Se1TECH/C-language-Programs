a7f934e4670ea1c93efbf96ed88d3ab631a2460d
7Tech <119029097+Se1TECH@users.noreply.github.com>
2022-12-13 16:43:14 +0530

    Create Print table using function.c

---
+++ Print table using function.c
@@ -0,0 +1,22 @@
+//Question: Write a Program in C to Print Table of Number using Function.
+#include<stdio.h>
+#include<conio.h>
+void table(int);
+void main()
+{
+	int i;
+	clrscr();
+	printf("Enter number for table : ");
+	scanf("%d",&i);
+	table(i);
+	getch();
+}
+
+void table(int i)
+{
+	int a;
+	for(a=1;a<=10;a++)
+	{
+		printf("%d\t*\t%d\t=%d\n",i,a,a*i);
+	}
+}
