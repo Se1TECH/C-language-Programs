1c1097b9948562a00c479347b1c6c4f2f169399e
7Tech <119029097+Se1TECH@users.noreply.github.com>
2022-12-15 22:58:54 +0530

    Create string in reverse using function.c

---
+++ string in reverse using function.c
@@ -0,0 +1,17 @@
+//Question: Write Program in C to Print String in Reverse using Function.
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void reverse(char a[20])
+{
+	printf("\nYour reverse String is :- %s",strrev(a));
+}
+void main()
+{
+	char a[20];
+	clrscr();
+	printf("Enter your string:- ");
+	scanf("%s",&a);
+	reverse(a);
+	getch();
+}
